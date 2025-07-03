106161849083a06c3ff44412e4260ad3b1087e33
Ikramullah-Jamali <ikramullahjamali12@gmail.com>
2025-07-03 15:30:46 +0500

    Create ConvertToUppercase.java

---
+++ UpperCase Method/ConvertToUppercase.java
@@ -0,0 +1,10 @@
+// Program to convert a string to uppercase using .toUpperCase() method
+class ConvertToUppercase  
+{
+    public static void main(String[] args) 
+    {
+        String name="student";
+        name=name.toUpperCase();
+        System.out.println("Uppercase name: "+name);
+    }
+}
