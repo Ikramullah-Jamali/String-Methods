ce55570eb84e54730efeffb3e3f1b3efb6d5df2a
Ikramullah-Jamali <ikramullahjamali12@gmail.com>
2025-07-03 15:26:37 +0500

    Create ConvertToLowercase .java

---
+++ LowerCase Method/ConvertToLowercase .java
@@ -0,0 +1,10 @@
+// Program to convert a string to lowercase using .toLowerCase() method
+public class ConvertToLowercase  
+{
+    public static void main(String[] args) 
+    {
+        String name="Ikram";
+        name=name.toLowerCase();
+        System.out.println("Lowercase name: " + name);
+    }
+}
