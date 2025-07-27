79f7f19292caed895869fae3ab7c004b249d18af
Ikramullah-Jamali <ikramullahjamali12@gmail.com>
2025-07-27 16:19:42 +0500

    Create StringEndsWithExample.java

---
+++ EndsWith Method/StringEndsWithExample.java
@@ -0,0 +1,18 @@
+
+// Program to demonstrate the use of .endsWith() method in Java
+
+public class StringEndsWithExample  
+{
+    public static void main(String [] args)  
+    {
+        String name="Student";
+        if(name.endsWith("ent"))
+        {
+            System.out.println("Correct.");
+        }
+        else
+        {
+            System.out.println("Wrong.");
+        }
+    }
+}
