3c9d86a28ca638bbb210ee6f20b67c5a651c11e6
Ikramullah-Jamali <ikramullahjamali12@gmail.com>
2025-07-27 16:16:31 +0500

    Create  SearchSubstringFromIndex.java

---
+++ IndexOf Method/SearchSubstringFromIndex.java
@@ -0,0 +1,14 @@
+
+public class SearchSubstringFromIndex 
+{
+    public static void main(String [] args)
+    {
+        String name="daniyaldaniyal";
+        // Find the index of "al" starting from index 10
+        System.out.println("From Index: "+name.indexOf("al",10));
+
+        // Find the index of 'a' starting from index 7
+        System.out.println("From Index: "+name.indexOf('a',7));
+
+    }
+}
