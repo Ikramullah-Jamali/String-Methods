8db52a3d1694b2f1de562242907ae65b068a1172
Ikramullah-Jamali <ikramullahjamali12@gmail.com>
2025-07-27 16:18:15 +0500

    Create StringCharAtExample.java

---
+++ CharAt Method/StringCharAtExample.java
@@ -0,0 +1,13 @@
+
+// Program to demonstrate the use of .charAt() method in Java
+public class StringCharAtExample  
+{
+    public static void main(String [] args)
+    {
+        String Name="Ikram";
+        for(int i=0;i<Name.length();i++)
+        {
+            System.out.print(Name.charAt(i)+" ");
+        }
+    }
+}
