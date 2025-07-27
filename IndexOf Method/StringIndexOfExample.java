498a536f244a670848e18f35f117e309a804698c
Ikramullah-Jamali <ikramullahjamali12@gmail.com>
2025-07-27 16:20:47 +0500

    Create StringIndexOfExample.java

---
+++ IndexOf Method/StringIndexOfExample.java
@@ -0,0 +1,11 @@
+
+// Program to demonstrate the use of .indexOf() method in Java
+
+public class StringIndexOfExample  
+{
+    public static void main(String [] args)
+    {
+        String name="Student";
+        System.out.println("First Index: "+name.indexOf("n"));
+    }
+}
