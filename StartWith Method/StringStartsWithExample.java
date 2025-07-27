392312a1decf2ef0efab4db6e94d297fbaac1035
Ikramullah-Jamali <ikramullahjamali12@gmail.com>
2025-07-27 16:21:59 +0500

    Create StringStartsWithExample.java

---
+++ StartWith Method/StringStartsWithExample.java
@@ -0,0 +1,18 @@
+
+// Program to demonstrate the use of .startsWith() method in Java
+
+public class StringStartsWithExample  
+{
+    public static void main(String [] args)
+    {
+        String name="Student";
+        if(name.startsWith("St"))
+        {
+            System.out.println("Correct.");
+        }
+        else
+        {
+            System.out.println("Wrong.");
+        }
+    }
+}
