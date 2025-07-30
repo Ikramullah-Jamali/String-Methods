27d24d1a970bfdd29a5a69e796d128d33cf909ca
Ikramullah-Jamali <ikramullahjamali12@gmail.com>
2025-07-30 13:55:37 +0500

    Create StringEqualsExample.java

---
+++ Equals Method/StringEqualsExample.java
@@ -0,0 +1,10 @@
+public class StringEqualsExample
+{
+    public static void main(String[] args) 
+  {
+    
+    String name1="khan";
+    String name2="khan";
+    System.out.println(name1.equals(name2));
+  } 
+} 
