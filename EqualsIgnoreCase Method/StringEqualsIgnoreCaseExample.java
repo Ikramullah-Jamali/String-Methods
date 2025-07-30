00e3f2cbcfa5ee7baa84ff6060821ae7f1bd9ec1
Ikramullah-Jamali <ikramullahjamali12@gmail.com>
2025-07-30 13:59:56 +0500

    Create StringEqualsIgnoreCaseExample.java

---
+++ EqualsIgnoreCase Method/StringEqualsIgnoreCaseExample.java
@@ -0,0 +1,11 @@
+
+public class StringEqualsIgnoreCaseExample 
+{     
+    public static void main(String[] args) 
+    {
+        String name1="khan";
+        String name2="KHAN";
+        System.out.println(name1.equalsIgnoreCase(name2));
+    }  
+
+}
